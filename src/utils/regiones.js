1e15e660f9a903a540cafce413618743766fb16e
reynaldo <reynaldoqs@gmail.com>
2020-08-24 15:21:22 -0400

    add deploy

--- src/utils/regiones.js
+++ src/utils/regiones.js
@@ -182,14 +182,375 @@ export const REGIONES = [
   {
     label: "REGION DE VALPARAISO",
     value: "5",
+    comunas: [
+      {
+        label: "ISLA DE PASCUA",
+        value: "5101",
+      },
+      {
+        label: "LA LIGUA",
+        value: "5201",
+      },
+      {
+        label: "PETORCA",
+        value: "5202",
+      },
+      {
+        label: "CABILDO",
+        value: "5203",
+      },
+      {
+        label: "ZAPALLAR",
+        value: "5204",
+      },
+      {
+        label: "PAPUDO",
+        value: "5205",
+      },
+      {
+        label: "VALPARAISO",
+        value: "5301",
+      },
+      {
+        label: "VINA DEL MAR",
+        value: "5302",
+      },
+      {
+        label: "VILLA ALEMANA",
+        value: "5303",
+      },
+      {
+        label: "QUILPUE",
+        value: "5304",
+      },
+      {
+        label: "CASABLANCA",
+        value: "5305",
+      },
+      {
+        label: "QUINTERO",
+        value: "5306",
+      },
+      {
+        label: "PUCHUNCAVI",
+        value: "5307",
+      },
+      {
+        label: "JUAN FERNANDEZ",
+        value: "5308",
+      },
+      {
+        label: "CON CON",
+        value: "5309",
+      },
+      {
+        label: "SAN ANTONIO",
+        value: "5401",
+      },
+      {
+        label: "SANTO DOMINGO",
+        value: "5402",
+      },
+      {
+        label: "CARTAGENA",
+        value: "5403",
+      },
+      {
+        label: "EL TABO",
+        value: "5404",
+      },
+      {
+        label: "EL QUISCO",
+        value: "5405",
+      },
+      {
+        label: "ALGARROBO",
+        value: "5406",
+      },
+      {
+        label: "QUILLOTA",
+        value: "5501",
+      },
+      {
+        label: "NOGALES",
+        value: "5502",
+      },
+      {
+        label: "HIJUELAS",
+        value: "5503",
+      },
+      {
+        label: "LA CALERA",
+        value: "5504",
+      },
+      {
+        label: "LA CRUZ",
+        value: "5505",
+      },
+      {
+        label: "LIMACHE",
+        value: "5506",
+      },
+      {
+        label: "OLMUE",
+        value: "5507",
+      },
+      {
+        label: "SAN FELIPE",
+        value: "5601",
+      },
+      {
+        label: "PANQUEHUE",
+        value: "5602",
+      },
+      {
+        label: "CATEMU",
+        value: "5603",
+      },
+      {
+        label: "PUTAENDO",
+        value: "5604",
+      },
+      {
+        label: "SANTA MARIA",
+        value: "5605",
+      },
+      {
+        label: "LLAY-LLAY",
+        value: "5606",
+      },
+      {
+        label: "LOS ANDES",
+        value: "5701",
+      },
+      {
+        label: "CALLE LARGA",
+        value: "5702",
+      },
+      {
+        label: "SAN ESTEBAN",
+        value: "5703",
+      },
+      {
+        label: "RINCONADA",
+        value: "5704",
+      },
+    ],
   },
   {
     label: "REGION DEL LIBERTADOR BERNARDO O'HIGGINS",
     value: "6",
+    comunas: [
+      {
+        label: "RANCAGUA",
+        value: "6101",
+      },
+      {
+        label: "MACHALI",
+        value: "6102",
+      },
+      {
+        label: "GRANEROS",
+        value: "6103",
+      },
+      {
+        label: "SAN FRANCISCO DE MOSTAZAL",
+        value: "6104",
+      },
+      {
+        label: "DONIHUE",
+        value: "6105",
+      },
+      {
+        label: "COLTAUCO",
+        value: "6106",
+      },
+      {
+        label: "CODEGUA",
+        value: "6107",
+      },
+      {
+        label: "PEUMO",
+        value: "6108",
+      },
+      {
+        label: "LAS CABRAS",
+        value: "6109",
+      },
+      {
+        label: "SAN VICENTE",
+        value: "6110",
+      },
+      {
+        label: "PICHIDEGUA",
+        value: "6111",
+      },
+      {
+        label: "RENGO",
+        value: "6112",
+      },
+      {
+        label: "REQUINOA",
+        value: "6113",
+      },
+      {
+        label: "OLIVAR",
+        value: "6114",
+      },
+      {
+        label: "MALLOA",
+        value: "6115",
+      },
+      {
+        label: "COINCO",
+        value: "6116",
+      },
+      {
+        label: "QUINTA TILCOCO",
+        value: "6117",
+      },
+      {
+        label: "SAN FERNANDO",
+        value: "6201",
+      },
+      {
+        label: "CHIMBARONGO",
+        value: "6202",
+      },
+      {
+        label: "NANCAGUA",
+        value: "6203",
+      },
+      {
+        label: "PLACILLA",
+        value: "6204",
+      },
+      {
+        label: "SANTA CRUZ",
+        value: "6205",
+      },
+      {
+        label: "LOLOL",
+        value: "6206",
+      },
+      {
+        label: "PALMILLA",
+        value: "6207",
+      },
+      {
+        label: "PERALILLO",
+        value: "6208",
+      },
+      {
+        label: "CHEPICA",
+        value: "6209",
+      },
+      {
+        label: "PUMANQUE",
+        value: "6214",
+      },
+      {
+        label: "PICHILEMU",
+        value: "6301",
+      },
+      {
+        label: "NAVIDAD",
+        value: "6302",
+      },
+      {
+        label: "LITUECHE",
+        value: "6303",
+      },
+      {
+        label: "LA ESTRELLA",
+        value: "6304",
+      },
+      {
+        label: "MARCHIGUE",
+        value: "6305",
+      },
+      {
+        label: "PAREDONES",
+        value: "6306",
+      },
+    ],
   },
   {
     label: "REGION DEL MAULE",
     value: "7",
+    comunas: [
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+      {
+        label: "ISLA",
+        value: "5101",
+      },
+    ],
   },
   {
     label: "REGION DEL BIO BIO",
